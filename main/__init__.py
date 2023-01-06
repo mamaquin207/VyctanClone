a5e64fe585140268838455025f96e7c27b8b3dbf
mamaquin207 <76276035+mamaquin207@users.noreply.github.com>
2023-01-05 23:43:12 -0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = 28522624
+API_HASH = "d9aabb5b9ba44df0f626321d484e9faa"
+BOT_TOKEN = "5723485979:AAEmuMveSVCkJ8t0rubLs015g8f4kCEd2GE"
+SESSION = "AQC8TAGPnGpg_wBdnvQIwNW-hNNEMkeI6Pho006pbrjN_nnTq1Ittvk2Kop9Td-8x4sShRderu79a0q5poRfD56x5cYbyMmptzhmuSdsEzElR3W9IUkY8o0jjgdRvY3vjGM9wmmuE3bT6nhfzDs8tpruV7yZ68EteGujPECeDoAK8oCzYZOy2mRYobU_skJ1D8nitgQeOs4zaaDqTJPWNvWnk243T4vG1eiHEHh9E7CNe0wssTshM5lNgPc2_AR8MrVTK0t_PjH1IN6501-nqf55RPntBjO7fVoJu75awfJ2tr7DrGs8ZHXmU14LDTLTOJjclMgea3VahP7GQaBt16ibAAAAAU0iIwkA"
+FORCESUB = "DroneBots"
+AUTH = 5589050121
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
